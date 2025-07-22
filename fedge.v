be504d925612c2f2a76ef12c9c43369044168c4c
Sam Roach <samuel.eu.roach@gmail.com>
2025-07-21 22:36:55 -0500

    Add files via upload

---
+++ fedge.v
@@ -0,0 +1,22 @@
+module fedge (
+	input wire clk, asynchIn,
+	output wire roll
+);
+
+reg a, b, c, falling;
+
+assign roll = falling;
+
+// synch chain
+always @ (posedge clk) begin
+	a <= asynchIn;
+	b <= a;
+	c <= b;
+end
+
+// falling edge detector
+always @ (*) begin
+	falling = c & ~b;
+end
+
+endmodule 
